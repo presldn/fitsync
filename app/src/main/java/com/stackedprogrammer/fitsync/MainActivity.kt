3ca78621f39401358e7c12978010b4568f3f73a8
presldn <ebonsu1996@yahoo.com>
2021-04-06 00:52:40 +0100

    main screen layout

--- app/src/main/java/com/stackedprogrammer/fitsync/MainActivity.kt
+++ app/src/main/java/com/stackedprogrammer/fitsync/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.stackedprogrammer.fitsync
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.drawee.view.SimpleDraweeView
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -13,6 +17,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +27,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mGoogleSignInClient: GoogleSignInClient
 
     private val reqCode: Int = 123
+
+    private lateinit var loginButton: Button
+    private lateinit var profileImage: SimpleDraweeView
+    private lateinit var emailAddress: TextView
+    private lateinit var fullName: TextView
 
     private val auth by lazy {
         FirebaseAuth.getInstance()
@@ -38,10 +48,14 @@ class MainActivity : AppCompatActivity() {
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
-        val loginButton: Button = findViewById(R.id.login)
+        loginButton = findViewById(R.id.login)
+        profileImage = findViewById(R.id.profile_image)
+        emailAddress = findViewById(R.id.profile_email_address)
+        fullName = findViewById(R.id.profile_full_name)
+
 
         loginButton.setOnClickListener {
-            if(GoogleSignIn.getLastSignedInAccount(this) != null) {
+            if (GoogleSignIn.getLastSignedInAccount(this) != null) {
                 logout()
             } else {
                 signInGoogle()
@@ -52,9 +66,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun logout() {
-        mGoogleSignInClient.signOut().addOnCompleteListener {
-            Toast.makeText(this, "Logged out.", Toast.LENGTH_LONG).show()
-        }
+        auth.signOut()
+        Toast.makeText(this, "Logged out.", Toast.LENGTH_LONG).show()
+        updateUI(auth.currentUser)
+
     }
 
     private fun signInGoogle() {
@@ -72,32 +87,58 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
         try {
-            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
-            if (account != null) {
-                updateUI(account)
-            }
+            val account = completedTask.getResult(ApiException::class.java)!!
+            firebaseAuthWithGoogle(account.idToken!!)
         } catch (e: ApiException) {
             Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun updateUI(account: GoogleSignInAccount) {
-        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-        auth.signInWithCredential(credential).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            loginButton.text = getString(R.string.logout)
+            setImageProfile(user.photoUrl)
 
-                Toast.makeText(this, "Logged In successfully!", Toast.LENGTH_SHORT).show()
+            fullName.text = user.displayName
 
+            emailAddress.visibility = View.VISIBLE
+            emailAddress.text = user.email
 
-            } else {
-                // If sign in fails, display a message to the user.
-                Log.w(tag, "signInWithCredential:failure", task.exception)
-            }
+        } else {
+            emailAddress.visibility = View.GONE
+            loginButton.text = getString(R.string.login)
         }
+    }
+
+    private fun setImageProfile(photoUrl: Uri?) {
+        if (photoUrl != null) {
+            profileImage.setImageURI(photoUrl.toString())
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(tag, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(tag, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
     }
 
     override fun onStart() {
         super.onStart()
+
+        val user = auth.currentUser
+        updateUI(user)
+
         if (GoogleSignIn.getLastSignedInAccount(this) != null) {
             Toast.makeText(this, "Already logged in.", Toast.LENGTH_SHORT).show()
         }
